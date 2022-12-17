519e712c1b2de19ba18ae3772ba11f561e9bd750
Vigneshwaran Elangovan <vygneshwaran@gmail.com>
2022-12-17 23:59:01 +0530

    charger base design

--- app/src/main/java/com/vickycodes/glyph/MainActivity.kt
+++ app/src/main/java/com/vickycodes/glyph/MainActivity.kt
@@ -3,13 +3,10 @@ package com.vickycodes.glyph
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.vickycodes.glyph.ui.component.ChargerCurve
 import com.vickycodes.glyph.ui.theme.GlyphTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,10 +15,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GlyphTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                ChargerCurve()
             }
         }
     }
---
+++ app/src/main/java/com/vickycodes/glyph/ui/component/ChargerCurve.kt
@@ -0,0 +1,106 @@
+package com.vickycodes.glyph.ui.component
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+private const val TAG = "CHARGER_CURVE"
+
+@Composable
+internal fun ChargerCurve() {
+
+
+    Column() {
+
+        Canvas(modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .clipToBounds()) {
+
+            val canvasWidth = size.width
+            val canvasHeight = size.height
+            drawArc(
+                brush = SolidColor(Color.Black),
+                startAngle = -180f,
+                sweepAngle = 180f,
+                useCenter = false,
+                style = Stroke(35f)
+            )
+
+            drawLine(
+                brush = SolidColor(Color.Black),
+                start = Offset(x = 0f, y = canvasHeight / 2),
+                end = Offset(x = 0f, y = canvasHeight),
+                strokeWidth = 35f
+            )
+        }
+
+        Canvas(modifier = Modifier
+            .width(10.dp)
+            .height(200.dp)
+            .clipToBounds()) {
+
+            val canvasWidth = size.width
+            val canvasHeight = size.height
+
+            drawLine(
+                brush = SolidColor(Color.Black),
+                start = Offset(x = 0f, y = 0f),
+                end = Offset(x = 0f, y = canvasHeight),
+                strokeWidth = 35f
+            )
+        }
+
+
+
+        Canvas(modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .clipToBounds()) {
+
+            val canvasWidth = size.width
+            val canvasHeight = size.height
+
+
+            drawArc(
+                brush = SolidColor(Color.Black),
+                startAngle = -0f,
+                sweepAngle = 180f,
+                useCenter = false,
+                style = Stroke(35f)
+            )
+
+            drawLine(
+                brush = SolidColor(Color.Black),
+                start = Offset(x = 0f, y = 0f),
+                end = Offset(x = 0f, y = canvasHeight / 2),
+                strokeWidth = 35f
+            )
+
+            drawLine(
+                brush = SolidColor(Color.Black),
+                start = Offset(x = canvasWidth, y = 0f),
+                end = Offset(x = canvasWidth, y = canvasHeight / 2),
+                strokeWidth = 35f
+            )
+
+        }
+
+    }
+
+}
+
+
+@Preview
+@Composable
+private fun PreviewChargerCurve() {
+    ChargerCurve()
+}
